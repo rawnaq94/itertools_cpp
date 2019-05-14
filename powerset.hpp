2140b5c9cd9ce675d2715d13d3417a51510d058b
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-05-14 13:22:36 +0300

    Create powerset.hpp

---
+++ powerset.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace itertools
+{
+
+	template<typename T1> class powerset
+  {//Represents all subgroups of iterable
+		
+	
+		public:
+		 	powerset<T1>(const T1 x): a(x) {}
+    
+     private:
+			T1 a;
+		      
+				class iterator
+        {
+
+				};
+        
+        
+		iterator start() 
+    {
+			return NULL;
+		}
+			
+		iterator stop()
+    {
+			return NULL;
+		}
+	};
+}
