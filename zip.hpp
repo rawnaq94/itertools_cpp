1ab5013f68c5023dc6798fbd8b7faf6f3492db75
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-05-14 13:04:10 +0300

    Create zip.hpp

---
+++ zip.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+namespace itertools
+{
+	template<typename T1, typename T2> class zip
+  {
+		
+		public:
+			zip<T1,T2>(const T1 x, const T2 y) : a(x),b(y){}
+		private:
+			T1 a;
+			T2 b;
+	
+  
+			class iterator
+      {
+	      typename T1::iterator x1Start;
+				typename T1::iterator x1Stop;
+				typename T2::iterator y2Start;
+				typename T2::iterator y2Stop;
+
+
+			public:
+				iterator(typename T1::iterator xStart, typename T1::iterator xStop, typename T2::iterator yStart, typename T2::iterator yStop):
+				x1Start(xStart), x1Stop(xStop),
+				y2Start(yStart), y2Stop(yStop)
+			  {}
+        auto operator*() const
+        {
+					ostringstream stream;
+					stream<<*x1Start<<","<<*y2Start;
+					return stream.str();
+				}
+
+				iterator& operator++()
+        {
+					++x1Start;
+					++y2Start;
+					return *this;
+				}
+
+				const iterator operator++(int)
+        {
+					iterator Cd(*this);
+					operator++();
+					return Cd;
+				}
+
+      	bool operator!=(const iterator& our) const
+        {
+					if(*this==our)
+						return false;
+					return true;
+				} 
+        
+				bool operator==(const iterator& other) const
+        {
+					if(x1Start == other.x1Start)
+						if(y2Start == other.y2Start)
+							return  true;
+					return false;
+				}
+
+		}; 
+		
+		typename zip<T1,T2>::iterator start()
+    {
+			return zip<T1,T2>::iterator(a.start(), a.stop(), b.start(), b.stop());
+		}
+			
+		typename zip<T1,T2>::iterator stop()
+    {
+			return zip<T1,T2>::iterator(a.stop(), a.stop(), b.stop(), b.stop());
+		}
+
+	}; 
+
+}
