13970a3645ff216305b9c82c0146248b794bce21
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-05-14 14:52:11 +0300

    Update product.hpp

--- product.hpp
+++ product.hpp
@@ -2,83 +2,62 @@
 
 namespace itertools
 {
-
-	template<typename T1, typename T2> class product
-  {//Represents a cartesian product of two iterable
-                private:
-			T1 a;
-			T2 b;
-		public:
-			product<T1,T2>(const T1 x, const T2 y) : a(x) , b(y){}
-		
-    class iterator {
-
-			public:
-				iterator(typename T1::iterator tIt, typename T1::iterator tIy2Stop, typename T2::iterator sIt, typename T2::iterator sIy2Stop):
-					x1Start(tIt), y2Stop(tIy2Stop),
-					sStart(sIt), y2Start(sIt), sStop(sIy2Stop)
-				 {}
-
-				auto operator*() const
-        {
-					ostringstream stream;
-					stream<<*x1Start<<","<<*y2Start;
-					return stream.str();
-				}
-
-				iterator& operator++()
-        {
-					++y2Start;
-					if (y2Start == sStop)
-          {
-						++x1Start;
-						if (x1Start != y2Stop)
-							y2Start = sStart;
-					}	
-					return *this;
-				 }
-
-				const iterator operator++(int)
-        {
-					iterator Cd(*this);
-					operator++();
-					return Cd;
-				}
-
-      	bool operator!=(const iterator& other) const
-        {
-					if(*this==other)
-						return false;
-					return true;
-				}
+    template <typename T1, typename T2>
+    class product 
+    {//Represents a Cartesian product of iterable.
+        private:
+        T1 firstIt;
+        T2 socundIt;
         
-				bool operator==(const iterator& other) const
+        public:
+        product(T1 bigen, T2 end) : firstIt(bigen), socundIt(end)
+	{
+        }
+        
+        template <typename X, typename Y>
+        class iterator
         {
-					if (x1Start == other.x1Start)
-						if(y2Start == other.y2Start)
-							return true;
-					return false;
-				}
+          private:
+            X firstIndex;
+            Y lastIndex;
 
-			
-      private:
-				typename T1::iterator x1Start;
-        typename T2::iterator y2Start;
-				typename T1::iterator y2Stop;
-				typename T2::iterator sStart;
-				typename T2::iterator sStop;
-		};
-		
-		typename product<T1,T2>::iterator begin()
-    {
-			return product<T1,T2>::iterator(a.begin(), a.end(),b.begin(),b.end());
-		}
-			
-		typename product<T1,T2>::iterator end() 
-    {
-			return product<T1,T2>::iterator(a.end(), a.end(),b.end(),b.end());
-		}
+            public:
+            iterator(X iter1, Y iter2) : firstIndex(iter1), lastIndex(iter2)
+	    {
+            }
 
-	}; 
+            std::pair<decltype(*firstIndex),decltype(*lastIndex)> operator*() const 
+	    {
+             return  std::pair<decltype(*firstIndex),decltype(*lastIndex)> (*firstIndex , *lastIndex);
+            }
 
+            iterator<X, Y>& operator++()
+	    {
+	     return *this;
+            }
+
+	    bool operator==(iterator<X,Y> it) const
+	    {
+	     return false;
+	    }
+
+            bool operator!=(iterator<X,Y> it) const
+	    {
+	     return false;
+            }
+        };
+
+        public:
+
+        auto begin()
+        {
+            return iterator <decltype(firstIt.begin()),decltype(socundIt.begin())> (firstIt.begin(), socundIt.begin());;
+        }
+
+        auto end()
+        {
+            return iterator <decltype(firstIt.end()),decltype(socundIt.end())> (firstIt.end(), socundIt_it2.end());;
+        }
+
+    };
 }
