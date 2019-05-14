2f647f7d1720547285622abb01a20cf4f974f806
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-05-14 12:50:07 +0300

    Create chain.hpp

---
+++ chain.hpp
@@ -0,0 +1,90 @@
+#pragma once
+
+#import "range.hpp"
+
+namespace itertools
+{
+	
+	  template<typename T1, typename T2>
+   	class chain
+    {//Represents a concatenation of two iterable.
+
+      	public:
+	     	chain<T1,T2>( T1 x,  T2 y) //x:first iterable , y:secound iterable
+		   	: a(x),
+			  b(y) {}
+        
+        private:
+	    	T1 a;
+	    	T2 b;
+		
+
+	 	class iterator
+    {
+
+			public: 
+				 iterator(typename T1::iterator xStart, typename T1::iterator xStop, typename T2::iterator yStart,typename T2::iterator yStop):
+				 x1Start(xStart), x1Stop(xStop),
+         y2Start(yStart), y2Stop(yStop) {}
+ 				 auto operator*() const
+         {
+					if (x1Start!=x1Stop)
+						return *x1Start;
+					else return *y2Start;
+  			 }
+
+				 iterator& operator++()
+         {
+					if(x1Start!=x1Stop)
+						++x1Start;
+					else
+						++y2Start;
+					return *this;	
+				}
+
+				const iterator operator++(int)
+        {
+					iterator _cd(*this);
+					operator++();
+					return _cd;
+				}
+
+				bool operator==(const iterator& other) const
+        {
+					if(x1Start==other.x1Start) 
+						if(y2Start==other.y2Start)
+							return true;
+					return false;
+				}
+
+				bool operator!=(const iterator& other) const
+        {
+					if(*this==other)
+						return false;
+					return true;
+				}
+
+        private:
+				typename T1::iterator x1Start;
+				typename T1::iterator x1Stop;
+				typename T2::iterator y2Start;
+        typename T2::iterator y2Stop;
+		};
+
+
+
+
+         typename chain<T1,T2>::  iterator start()
+         {
+		   	 return chain<T1,T2>::iterator(a.start(), a.stop(), b.start());
+	       }
+			
+    		typename chain<T1,T2>::  iterator stop()
+        {
+			  return chain<T1,T2>::iterator(a.stop(), a.stop(), b.stop());
+	      }
+
+	};
+
+
+}
