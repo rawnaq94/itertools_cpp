ace9f125916a9b1884e275e3378270da28879d07
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-05-14 15:46:42 +0300

    Update product.hpp

--- product.hpp
+++ product.hpp
@@ -5,55 +5,59 @@ namespace itertools
     class product 
     {
         private:
-        T1 _it1;
-        T2 _it2;
+        T1 firstIt;
+        T2 lastIt;
         
         public:
-        product(T1 _start, T2 _end) : _it1(_start), _it2(_end) {
-
+        product(T1 bigenN, T2 endD) : firstIt(bigenN), lastIt(endD)
+	{
         }
         
         template <typename P1, typename P2>
         class iterator
         {
           private:
-            P1 data1;
-            P2 data2;
+            P1 firstIndex;
+            P2 lastIndex;
 
             public:
-            iterator(P1 ptr1, P2 ptr2) : data1(ptr1), data2(ptr2) {
-
+            iterator(P1 temp1, P2 temp2) : firstIndex(temp1), lastIndex(temp2) 
+	    {
             }
 
-            std::pair<decltype(*data1),decltype(*data2)> operator*() const {
-
-             return  std::pair<decltype(*data1),decltype(*data2)> (*data1 , *data2);
-}
-
-            iterator<P1, P2>& operator++() {
-
-			    return *this;
+            std::pair<decltype(*firstIndex),decltype(*lastIndex)> operator*() const
+	    {
+             return  std::pair<decltype(*firstIndex),decltype(*lastIndex)> (*firstIndex , *lastIndex);
             }
 
-		    bool operator==(iterator<P1,P2> it) const {
-			    return false;
-		    }
-
-		    bool operator!=(iterator<P1,P2> it) const {
-			    return false;
+            iterator<P1, P2>& operator++()
+	    {
+	      return *this;
             }
+
+		
+	   bool operator!=(iterator<P1,P2> it) const 
+	   {
+	      return false;
+           }
+		
+	    bool operator==(iterator<P1,P2> it) const
+	    {
+	      return false;
+	    }
+
         };
 
         public:
 
         auto begin()
         {
-            return iterator <decltype(_it1.begin()),decltype(_it2.begin())> (_it1.begin(), _it2.begin());;
+            return iterator <decltype(firstIt.begin()),decltype(lastIt.begin())> (firstIt.begin(), lastIt.begin());;
         }
 
         auto end()
         {
-            return iterator <decltype(_it1.end()),decltype(_it2.end())> (_it1.end(), _it2.end());;
+            return iterator <decltype(firstIt.end()),decltype(lastIt.end())> (firstIt.end(), lastIt.end());;
         }
 
     };
